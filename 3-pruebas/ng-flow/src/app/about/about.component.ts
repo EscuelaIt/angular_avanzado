745085e6ce3b9567479823d9b100a5925ef2c508
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-04-18 10:48:05 +0200

    about click contact-form

--- 3-pruebas/ng-flow/src/app/about/about.component.ts
+++ 3-pruebas/ng-flow/src/app/about/about.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ab-about',
   template: `
-    <button (click)="byEmail=true">By Email</button>
-    <button (click)="byEmail=false">By Social Network</button>
+    <button name="contact-by-email" (click)="byEmail=true">By Email</button>
+    <button name="contact-by-social"(click)="byEmail=false">By Social Network</button>
     <aside *ngIf="byEmail ; then emailContact; else socialContact"></aside>
     <ng-template #emailContact>
       <ab-email></ab-email>
